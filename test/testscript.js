9d0cd237d61954d0f22bc4804866a5e69783c2b4
DavideDaniel <davidedaniel@gmail.com>
2015-01-25 02:56:51 -0500

    examples

--- test/testscript.js
+++ test/testscript.js
@@ -1,8 +1,17 @@
-var expect = require("chai").expect;
+var expect = require( "chai" )
+	.expect;
 
-describe("a simple test", function () {
-	it("should pass when true is true", function(){
-		expect(true).to.be.true;
-	})
-	// body...
-})
+describe( "a simple test", function () {
+	it( "should pass if true is true", function () {
+		expect( true )
+			.to.be.true;
+	} );
+} );
+
+describe( "simply add", function () {
+	it( "should add any 2 numbers as parameters", function () {
+		var num1 = 1
+		var num2 = 2
+		num1 + num2
+	} );
+} );
